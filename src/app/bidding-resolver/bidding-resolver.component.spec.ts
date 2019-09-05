fe19efda4a16b3be8d31da87472504fdcc836e3c
Anthony Barreau <anthony.barreau@itk.fr>
2019-09-05 12:04:56 +0200

    :bug: Fixes computeSecondPriceAuctionWinner not returning the correct price.

--- src/app/bidding-resolver/bidding-resolver.component.spec.ts
+++ src/app/bidding-resolver/bidding-resolver.component.spec.ts
@@ -26,16 +26,24 @@ describe('BiddingResolverComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should return second highest bid with multiple bids', () => {
-    expect(service.findSecondHighestBid([1, 32, 10])).toEqual(10);
+  it('should return second highest bid with multiple bids below reserve price', () => {
+    expect(service.findSecondHighestBid([1, 32, 10], 100)).toEqual(100);
+  });
+
+  it('should return second highest bid with multiple bids above reserve price', () => {
+    expect(service.findSecondHighestBid([110, 32, 200], 100)).toEqual(110);
   });
 
   it('should return second highest bid with no bids', () => {
-    expect(service.findSecondHighestBid([])).toBeUndefined();
+    expect(service.findSecondHighestBid([], 100)).toBeUndefined();
   });
 
-  it('should return second highest bid with one bid', () => {
-    expect(service.findSecondHighestBid([1])).toEqual(1);
+  it('should return second highest bid with one bid below reserve price', () => {
+    expect(service.findSecondHighestBid([1], 100)).toEqual(100);
+  });
+
+  it('should return second highest bid with one bid above reserve price', () => {
+    expect(service.findSecondHighestBid([200], 100)).toEqual(200);
   });
 
   it('should find highest bidder with multiple bidders', () => {
@@ -74,6 +82,38 @@ describe('BiddingResolverComponent', () => {
       winner: 'B',
       highestBidPerWinner: new Map().set('B', 0)
     });
+  });
+
+  it('should compute second price auction winner with multiple bidders', () => {
+    const bidders = new Map<string, number[]>()
+      .set('A', [ 110, 130 ])
+      .set('B', [ 0 ])
+      .set('C', [ 125 ])
+      .set('E', [ 132, 135, 140 ])
+      .set('D', [ 105, 115, 90 ]);
+
+    service.computeSecondPriceAuctionWinner(bidders, 100);
+  });
+
+  it('should compute second price auction winner with one bidder with reserve price unreached', () => {
+    const bidders = new Map<string, number[]>().set('B', [ 0 ]);
+    expect(service.computeSecondPriceAuctionWinner(bidders, 100)).toEqual({
+      bidder: 'B',
+      price: 100
+    });
+  });
+
+  it('should compute second price auction winner with one bidder with reserve price reached', () => {
+    const bidders = new Map<string, number[]>().set('B', [ 110 ]);
+    expect(service.computeSecondPriceAuctionWinner(bidders, 100)).toEqual({
+      bidder: 'B',
+      price: 110
+    });
+  });
+
+  it('should compute second price auction winner with no bidder', () => {
+    const bidders = new Map<string, number[]>();
+    expect(service.computeSecondPriceAuctionWinner(bidders, 100)).toBeUndefined();
   });
 
 
--- src/app/bidding-resolver/bidding-resolver.service.ts
+++ src/app/bidding-resolver/bidding-resolver.service.ts
@@ -19,13 +19,16 @@ export class BiddingResolverService {
 
   extractHighestBidFromBiddersWithWinner(bidders: Map<string, number[]>): HighestBidsWithWinner {
     let highestBidder: { bidder: string, highestBid: number };
+    const highestBidPerUser = new Map<string, number>();
 
     bidders.forEach((bids: number[], bidder: string) => {
-      const highestBidOfCurrentBidder = bids.sort().pop();
+      const highestBidOfCurrentBidder = bids.sort((a: number, b: number) => a - b)[bids.length - 1];
 
       if (!highestBidder || highestBidder.highestBid < highestBidOfCurrentBidder) {
         highestBidder = { bidder, highestBid: highestBidOfCurrentBidder };
       }
+
+      highestBidPerUser.set(bidder, highestBidOfCurrentBidder);
     });
 
     return {
@@ -34,23 +37,28 @@ export class BiddingResolverService {
     };
   }
 
-  findSecondHighestBid(bids: number[]): number {
+  findSecondHighestBid(bids: number[], reservePrice): number {
     if (bids.length === 0) {
       return;
     }
 
     const bidsOrderByDesc =  bids.sort((a: number, b: number) => b - a);
+    const secondHighBid = bidsOrderByDesc.length > 1 ? bidsOrderByDesc[ 1 ] : bidsOrderByDesc[ 0 ];
 
-    return bidsOrderByDesc.length > 1 ? bidsOrderByDesc[ 1 ] : bidsOrderByDesc[ 0 ];
+    return secondHighBid >= reservePrice ? secondHighBid : reservePrice;
   }
 
   computeSecondPriceAuctionWinner(bidders: Map<string, number[]>, reservePrice: number): { bidder: string, price: number} {
     const highestBidsWithWinner = this.extractHighestBidFromBiddersWithWinner(bidders);
     const flattenBids = Array.from(highestBidsWithWinner.highestBidPerWinner.values());
 
+    if (!highestBidsWithWinner || !flattenBids || flattenBids.length === 0) {
+      return;
+    }
+
     return {
       bidder: highestBidsWithWinner.winner,
-      price: this.findSecondHighestBid(flattenBids)
+      price: this.findSecondHighestBid(flattenBids, reservePrice)
     };
   }
 
